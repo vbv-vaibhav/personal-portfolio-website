ba6b953b589ff5bfa8966a5aa754ab8535abcb02
Vaibhav Bansal <vbvbansal@gmail.com>
2024-04-15 11:04:47 +0530

    added portfolio sc link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,22 @@ const Footer = () => {
   return (
     <div className="bg-[#222831]">
       <div className="max-w-[1100px] sm:h-[120px] p-4 flex justify-between mx-auto">
-        <a
-          href="/"
-          className="text-[#76ABAE] mt-6 hover:text-gray-300 duration-300"
-        >
-          Vaibhav Bansal
-        </a>
+        <div className="flex flex-col">
+          <a
+            href="/"
+            className="text-[#76ABAE] mt-6 hover:text-gray-300 duration-300 text-center"
+          >
+            Vaibhav Bansal
+          </a>
+          <a
+            href="https://github.com/vbv-vaibhav/personal-portfolio-website"
+            className="text-[#76ABAE] mt-1 hover:text-gray-300 duration-300 underline"
+            target="_blank"
+          >
+            Code for this portfolio
+          </a>
+        </div>
+
         <p className="text-[#76ABAE] mt-6">
           vbvbansal@gmail.com <br />
           New Delhi, India
